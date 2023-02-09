a69ecdc7c4622620fe06e242b47cc64d5ca43047
MoreTore <moretorque75@gmail.com>
2023-02-09 13:07:29 -0600

    added MRCC toggle in settings and updated mazdacan.py

--- common/params.cc
+++ common/params.cc
@@ -156,6 +156,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"NavSettingLeftSide", PERSISTENT},
     {"NavdRender", PERSISTENT},
     {"OpenpilotEnabledToggle", PERSISTENT},
+    {"OpenpilotLongitudinalControl", PERSISTENT},
     {"PandaHeartbeatLost", CLEAR_ON_MANAGER_START | CLEAR_ON_IGNITION_OFF},
     {"PandaSignatures", CLEAR_ON_MANAGER_START},
     {"Passive", PERSISTENT},
