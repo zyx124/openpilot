d0ea252855bba5a5afe576334689d1595f5f0843
MoreTore <moretorque75@gmail.com>
2023-06-03 00:40:48 -0500

    new-mazda params

--- common/params.cc
+++ common/params.cc
@@ -160,6 +160,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"ObdMultiplexingEnabled", CLEAR_ON_MANAGER_START | CLEAR_ON_ONROAD_TRANSITION},
     {"OpenpilotEnabledToggle", PERSISTENT},
     {"PandaHeartbeatLost", CLEAR_ON_MANAGER_START | CLEAR_ON_OFFROAD_TRANSITION},
+    {"OpenpilotLongitudinalControl", PERSISTENT},
     {"PandaSignatures", CLEAR_ON_MANAGER_START},
     {"Passive", PERSISTENT},
     {"PrimeType", PERSISTENT},
