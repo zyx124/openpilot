3757567b7a8f935c0e66fe3ed53ecc7785e5c0a3
MoreTore <moretorque75@gmail.com>
2023-07-05 17:53:12 -0500

    no mrcc

--- panda/board/safety/safety_mazda.h
+++ panda/board/safety/safety_mazda.h
@@ -29,7 +29,6 @@
 const CanMsg MAZDA_TX_MSGS[] = {{MAZDA_LKAS, 0, 8}, {MAZDA_CRZ_BTNS, 0, 8}, {MAZDA_LKAS2, 1, 8}, {MAZDA_LKAS_HUD, 0, 8}};
 
 AddrCheckStruct mazda_addr_checks[] = {
-  {.msg = {{MAZDA_CRZ_CTRL,     0, 8, .expected_timestep = 20000U}, { 0 }, { 0 }}},
   {.msg = {{MAZDA_CRZ_BTNS,     0, 8, .expected_timestep = 100000U}, { 0 }, { 0 }}},
   {.msg = {{MAZDA_STEER_TORQUE, 0, 8, .expected_timestep = 12000U}, { 0 }, { 0 }}},
   {.msg = {{MAZDA_ENGINE_DATA,  0, 8, .expected_timestep = 10000U}, { 0 }, { 0 }}},
@@ -72,7 +71,24 @@ static int mazda_rx_hook(CANPacket_t *to_push) {
     }
 
     // enter controls on rising edge of ACC, exit controls on ACC off
-    if (addr == MAZDA_CRZ_CTRL) {
+    //if (addr == MAZDA_CRZ_CTRL) {
+      //bool cruise_engaged = GET_BYTE(to_push, 0) & 0x8U;
+      //if (cruise_engaged) {
+        //if (!cruise_engaged_prev) {
+          //controls_allowed = 1;
+        //}
+      //} else {
+        //controls_allowed = 0;
+      //}
+      //cruise_engaged_prev = cruise_engaged;
+   // }
+
+    if (addr == MAZDA_ENGINE_DATA) {
+      gas_pressed = (GET_BYTE(to_push, 4) || (GET_BYTE(to_push, 5) & 0xF0U));
+    }
+
+    if (addr == MAZDA_PEDALS) {
+      brake_pressed = (GET_BYTE(to_push, 0) & 0x10U);
       bool cruise_engaged = GET_BYTE(to_push, 0) & 0x8U;
       if (cruise_engaged) {
         if (!cruise_engaged_prev) {
@@ -82,14 +98,6 @@ static int mazda_rx_hook(CANPacket_t *to_push) {
         controls_allowed = 0;
       }
       cruise_engaged_prev = cruise_engaged;
-    }
-
-    if (addr == MAZDA_ENGINE_DATA) {
-      gas_pressed = (GET_BYTE(to_push, 4) || (GET_BYTE(to_push, 5) & 0xF0U));
-    }
-
-    if (addr == MAZDA_PEDALS) {
-      brake_pressed = (GET_BYTE(to_push, 0) & 0x10U);
     }
 
     generic_rx_checks((addr == MAZDA_LKAS));
--- selfdrive/car/mazda/carstate.py
+++ selfdrive/car/mazda/carstate.py
@@ -95,8 +95,8 @@ class CarState(CarStateBase):
 
     # TODO: the signal used for available seems to be the adaptive cruise signal, instead of the main on
     #       it should be used for carState.cruiseState.nonAdaptive instead
-    ret.cruiseState.available = cp.vl["CRZ_CTRL"]["CRZ_AVAILABLE"] == 1
-    ret.cruiseState.enabled = cp.vl["CRZ_CTRL"]["CRZ_ACTIVE"] == 1
+    ret.cruiseState.available = True
+    ret.cruiseState.enabled = cp.vl["PEDALS"]["ACC_ACTIVE"] == 1
     ret.cruiseState.speed = cp.vl["CRZ_EVENTS"]["CRZ_SPEED"] * CV.KPH_TO_MS
 
     # On if no driver torque the last 5 seconds
@@ -146,11 +146,12 @@ class CarState(CarStateBase):
         ("LKAS_BLOCK", "STEER_RATE"),
         ("LKAS_TRACK_STATE", "STEER_RATE"),
         ("HANDS_OFF_5_SECONDS", "STEER_RATE"),
-        ("CRZ_ACTIVE", "CRZ_CTRL"),
-        ("CRZ_AVAILABLE", "CRZ_CTRL"),
+        #("CRZ_ACTIVE", "CRZ_CTRL"),
+        #("CRZ_AVAILABLE", "CRZ_CTRL"),
         ("CRZ_SPEED", "CRZ_EVENTS"),
         ("STANDSTILL", "PEDALS"),
         ("BRAKE_ON", "PEDALS"),
+        ("ACC_ACTIVE", "PEDALS"),
         ("BRAKE_PRESSURE", "BRAKE"),
         ("GEAR", "GEAR"),
         ("DRIVER_SEATBELT", "SEATBELT"),
