8062421597183aa812e6df709f34d75365190dcd
zyx <zhaoyuxin124@gmail.com>
2024-06-18 23:04:19 -0700

    merge from upstream

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/bash
 
 export PASSIVE="0"
+
 export ATHENA_HOST="wss://connect-ws.duckdns.org"
 export API_HOST="https://connect-api.duckdns.org"
+
+export FINGERPRINT="MAZDA CX-30"
+
 exec ./launch_chffrplus.sh
 
